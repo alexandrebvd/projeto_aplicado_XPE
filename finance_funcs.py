d566c2212befb464fe56ef7a1cf2a77f6ab3f511
David Aguina <aguinadavid@gmail.com>
2023-08-25 15:51:22 -0300

    Conecta as fórmulas financeiras à interface
    Utiliza o expander do streamlit para deixar a interface menos poluída,
    dessa forma o usuário pode escolher qual das calculadoras quer usar e
    abri-la.
    
    Adiciona também um função para converter string em umas lista de números.

---
+++ finance_funcs.py
@@ -0,0 +1,161 @@
+def calculate_simple_interest(principal, rate, time):
+    """
+    Calcula o juro simples.
+    
+    Args:
+    principal (float): O montante principal.
+    rate (float): A taxa de juro (em decimal).
+    time (float): O período de tempo (em anos).
+    
+    Returns:
+    float: O montante final após juro simples.
+    """
+    interest = principal * rate * time
+    return principal + interest
+
+def calculate_compound_interest(principal, rate, time, frequency):
+    """
+    Calcula o juro composto.
+    
+    Args:
+    principal (float): O montante principal.
+    rate (float): A taxa de juro (em decimal).
+    time (float): O período de tempo (em anos).
+    frequency (int): A frequência de capitalização por ano.
+    
+    Returns:
+    float: O montante final após juro composto.
+    """
+    n = frequency
+    amount = principal * (1 + rate/100 / n) ** (n * time)
+    return amount
+
+def calculate_annuity_payment(principal, rate, periods):
+    """
+    Calcula o valor dos pagamentos de uma anuidade (parcelas iguais).
+    
+    Args:
+    principal (float): O montante principal.
+    rate (float): A taxa de juro por período (em decimal).
+    periods (int): O número de períodos.
+    
+    Returns:
+    float: O valor do pagamento da anuidade.
+    """
+    payment = (principal * rate) / (1 - (1 + rate) ** -periods)
+    return payment
+
+def present_value_of_cash_flows(cash_flows, rate):
+    """
+    Calcula o valor presente de fluxos de caixa futuros.
+    
+    Args:
+    cash_flows (list): Uma lista de fluxos de caixa futuros.
+    rate (float): A taxa de desconto por período (em decimal).
+    
+    Returns:
+    float: O valor presente líquido dos fluxos de caixa.
+    """
+    pv = sum([cf / (1 + rate) ** idx for idx, cf in enumerate(cash_flows)])
+    return pv
+
+def calculate_future_value_of_annuity(payment, rate, periods):
+    """
+    Calcula o valor futuro de uma série de pagamentos (anuidade).
+
+    Args:
+    payment (float): Valor dos pagamentos periódicos.
+    rate (float): Taxa de juros por período (em decimal).
+    periods (int): Número de períodos.
+
+    Returns:
+    float: O valor futuro da anuidade.
+    """
+    fv = payment * ((1 + rate) ** periods - 1) / rate
+    return fv
+
+def calculate_roi(gains, costs):
+    """
+    Calcula o Retorno sobre Investimento (ROI).
+
+    Args:
+    gains (float): Ganhos provenientes do investimento.
+    costs (float): Custos do investimento.
+
+    Returns:
+    float: O ROI calculado.
+    """
+    roi = (gains - costs) * 100 / costs
+    return roi
+
+def calculate_break_even_point(fixed_costs, variable_costs, selling_price):
+    """
+    Calcula o Ponto de Equilíbrio em vendas.
+
+    Args:
+    fixed_costs (float): Custos fixos.
+    variable_costs (float): Custos variáveis por unidade.
+    selling_price (float): Preço de venda por unidade.
+
+    Returns:
+    float: O ponto de equilíbrio em unidades.
+    """
+    bep = fixed_costs / (selling_price - variable_costs)
+    return round(bep)
+
+def calculate_net_present_value(cash_flows, rate):
+    """
+    Calcula o Valor Presente Líquido (VPL) de um conjunto de fluxos de caixa.
+
+    Args:
+    cash_flows (list): Lista de fluxos de caixa futuros.
+    rate (float): Taxa de desconto por período (em decimal).
+
+    Returns:
+    float: O Valor Presente Líquido dos fluxos de caixa.
+    """
+    npv = sum([cf / (1 + rate) ** idx for idx, cf in enumerate(cash_flows)])
+    return npv
+
+def calculate_internal_rate_of_return(cash_flows):
+    """
+    Calcula a Taxa Interna de Retorno (TIR) de um conjunto de fluxos de caixa.
+
+    Args:
+    cash_flows (list): Lista de fluxos de caixa futuros.
+
+    Returns:
+    float: A Taxa Interna de Retorno calculada.
+    """
+    # Implementação da TIR usando métodos numéricos
+    pass
+
+def calculate_straight_line_depreciation(initial_value, salvage_value, useful_life):
+    """
+    Calcula a depreciação usando o método de linha reta.
+
+    Args:
+    initial_value (float): Valor inicial do ativo.
+    salvage_value (float): Valor residual do ativo.
+    useful_life (int): Vida útil do ativo em períodos.
+
+    Returns:
+    float: A depreciação por período.
+    """
+    depreciation = (initial_value - salvage_value) / useful_life
+    return depreciation
+
+def calculate_loan_payments(principal, rate, periods):
+    """
+    Calcula os pagamentos de um empréstimo (amortização).
+
+    Args:
+    principal (float): Montante principal do empréstimo.
+    rate (float): Taxa de juros por período (em decimal).
+    periods (int): Número de períodos.
+
+    Returns:
+    float: O valor dos pagamentos do empréstimo.
+    """
+    payment = (principal * rate) / (1 - (1 + rate) ** -periods)
+    return payment
--- pages/01_Calculadoras_e_Simuladores.py
+++ pages/01_Calculadoras_e_Simuladores.py
@@ -1,18 +1,133 @@
 import streamlit as st
+import finance_funcs as ff
+import re
 
 st.title("Calculadoras e simuladores")
 st.subheader("Essas são algumas calculadoras e simuladores que podem ajudar em seu aprendizado e planejamento financeiro.")
-st.header("Calculadora de juros compostos")
         
-def compound_interest(principal, rate, time):
-    return principal * (1 + rate / 100) ** time
+def text_to_list(text):
+    numbers_list = [int(i) for i in re.split("[^0-9]", text) if i != ""]
+    return numbers_list
 
-# Input parameters
-principal = st.number_input("Valor inicial", value=1000.0)
-rate = st.number_input("Taxa de juros anual (%)", value=10.0)
-time = st.number_input("Tempo (anos)", value=10.0)
+# Juros simples
+with  st.expander("Calculadora de juros simples"):
+    # Input parameters
+    principal = st.number_input("Valor inicial", value=500.0)
+    rate = st.number_input("Taxa de juros em decimal (Ex.:0.10)", value=0.10)
+    time = st.number_input("Tempo (anos)", value=5)
 
-result = compound_interest(principal, rate, time)
+    result = ff.calculate_simple_interest(principal, rate, time)
 
-# Display the final amount with larger font size
-st.markdown(f"<p style='font-size:36px;'>Total acumulado: ${result:.2f}</p>", unsafe_allow_html=True)
+    # Display the final amount with larger font size
+    st.markdown(f"<p style='font-size:24px;'>Total acumulado: R$ {result:.2f}</p>", unsafe_allow_html=True)
+
+
+# Juros compostos
+with st.expander("Calculadora de juros compostos"):
+    # Input parameters
+    principal = st.number_input("Valor inicial", value=1000.0)
+    rate = st.number_input("Taxa de juros anual (%)", value=10.0)
+    time = st.number_input("Tempo (anos)", value=10)
+    frequency = st.number_input("Frequência de capitalização por ano (meses)", value=12)
+
+    result = ff.calculate_compound_interest(principal, rate, time, frequency)
+
+    # Display the final amount with larger font size
+    st.markdown(f"<p style='font-size:24px;'>Total acumulado: R$ {result:.2f}</p>", unsafe_allow_html=True)
+
+
+# Pagamento de anuidade
+with st.expander("Calculadora de pagamento de anuidade"):
+    # Input parameters
+    principal = st.number_input("Valor inicial", value=900.0)
+    rate = st.number_input("Taxa de juros por período em decimal (Ex.: 0,10)", value=0.3)
+    periods = st.number_input("O número de períodos.", value=12)
+
+    result = ff.calculate_annuity_payment(principal, rate, periods)
+
+    st.markdown(f"<p style='font-size:24px;'>Valor do pagamento da anuidade: R$ {result:.2f}</p>", unsafe_allow_html=True)
+
+# Valor do fluxo de caixa
+with st.expander("Calculadora de valor presente de fluxos de caixa futuros"):
+    # Input parameters
+    str_cash_flows = st.text_input("Lista de fluxos de caixa futuros", value="1023.4, 784.5, 987.53")
+    cash_flows = text_to_list(str_cash_flows)
+    rate = st.number_input("A taxa de desconto por período (em decimal)", value=0.03)
+
+    result = ff.present_value_of_cash_flows(cash_flows, rate)
+
+    st.markdown(f"<p style='font-size:24px;'>Valor presente líquido dos fluxos de caixa: R$ {result:.2f}</p>", unsafe_allow_html=True)
+
+
+# Valor futuro da anuidade
+with st.expander("Calculadora de valor futuro da anuidade"):
+    # Input parameters
+    payment = st.number_input("Valor dos pagamentos periódicos", value=150.00)
+    rate = st.number_input("Taxa de juros por período (em decimal)", value=0.05)
+    periods = st.number_input("Número de períodos", value=5)
+    
+    result = ff.calculate_future_value_of_annuity(payment, rate, periods)
+
+    st.markdown(f"<p style='font-size:24px;'>Valor futuro da anuidade: R$ {result:.2f}</p>", unsafe_allow_html=True)
+
+# ROI
+with st.expander("Calculadora do ROI (Retorno sobre o investimento)"):
+    # Input parameters
+    gains = st.number_input("Ganhos provenientes do investimento", value=3000.00)
+    costs = st.number_input("Custos do investimento", value=2500.00)
+
+    result = ff.calculate_roi(gains, costs)
+
+    st.markdown(f"<p style='font-size:24px;'>Valor do ROI: {result:.2f}%</p>", unsafe_allow_html=True)
+    
+
+# Ponto de Break Even
+with st.expander("Calculadora de ponto de Break Even"):
+    # Input parameters
+    fixed_costs = st.number_input("Custos fixos", value=500.00)
+    variable_costs = st.number_input("Custos variáveis por unidade", value=2.50)
+    selling_price = st.number_input("Preço de venda por unidade", value=10.00)
+
+    result = ff.calculate_break_even_point(fixed_costs, variable_costs, selling_price)
+
+    st.markdown(f"<p style='font-size:24px;'>O ponto de equilíbrio em unidades: {result:.0f}</p>", unsafe_allow_html=True)
+    
+
+# Valor presente líquido
+with st.expander("Calculadora do valor presente líquido"):
+    # Input parameters
+    str_cash_flows = st.text_input("Lista de fluxos de caixa futuros", value="273.80, 579.90, 1030.50")
+    cash_flows = text_to_list(str_cash_flows)
+    rate = st.number_input("Taxa de desconto por período (em decimal)", value=0.15)
+
+    result = ff.calculate_net_present_value(cash_flows, rate)
+
+    st.markdown(f"<p style='font-size:24px;'>Valor Presente Líquido dos fluxos de caixa: {result:.2f}</p>", unsafe_allow_html=True)
+
+# Taxa interna de retorno
+with st.expander("Calculadora da taxa interna de retorno"):
+    st.markdown("Calculadora em construção")
+    
+
+# Depreciação linear
+with st.expander("Calculadora de depreciação linear"):
+    # Input parameters
+    initial_value = st.number_input("Valor inicial do ativo", value=33000.00)
+    salvage_value = st.number_input("Valor residual do ativo", value=15000.00)
+    useful_life = st.number_input("Vida útil do ativo em períodos", value=7)
+
+    result = ff.calculate_straight_line_depreciation(initial_value, salvage_value, useful_life)
+
+    st.markdown(f"<p style='font-size:24px;'>A depreciação por período: R$ {result:.2f}</p>", unsafe_allow_html=True)
+    
+
+# Pagamentos de empréstimos
+with st.expander("Calculadora de pagamentos de empréstimos"):
+    # Input parameters
+    principal = st.number_input("Montante principal do empréstimo", value=7000)
+    rate = st.number_input("Taxa de juros por período (em decimal)", value=0.08)
+    periods = st.number_input("Número de períodos", value=7)
+
+    result = ff.calculate_loan_payments(principal, rate, periods)
+
+    st.markdown(f"<p style='font-size:24px;'>Valor dos pagamentos do empréstimo: R$ {result:.2f}</p>", unsafe_allow_html=True)
