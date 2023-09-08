19a5eb1720ed68b22b6c031dca466068f90c4af2
Alexandre Daltro <alexandrebvd@gmail.com>
2023-09-07 21:19:17 -0300

    quiz pode ter entre 1 e 10 perguntas

--- pages/03_Quiz.py
+++ pages/03_Quiz.py
@@ -18,13 +18,18 @@ if 'quiz' not in sst:
     sst.quiz = False
 
 
-# Define função que gera o quiz com 5 perguntas escolhidas aleatórias do arquivo json.
-def gerar_quiz():
-    perguntas = q.quizGenerator()
+# Define função que gera o quiz com n perguntas escolhidas aleatórias do arquivo json.
+def gerar_quiz(n_perguntas=5):
+    perguntas = q.quizGenerator(n_perguntas)
     sst.perguntas = perguntas
     sst.corretas = 0
     sst.quiz = True
     return
+
+
+def resetar_quiz():
+    sst.quiz = False
+    sst.corretas = 0
 
 
 def verifica_alternativa():
@@ -39,24 +44,23 @@ def renderiza_questao(n):
         st.button(alternativa, use_container_width=True, key=alternativa, on_click=verifica_alternativa)
 
 
-st.subheader("Vamos treinar os conceitos sobre educação financeira!")
-col1, col2, col3 = st.columns(3)
+st.title("Vamos treinar os conceitos sobre educação financeira!")
+col1, col2 = st.columns(2)
 with col1:
-    st.title("Quiz")
+    n_perguntas = st.number_input('Número de questões (Máx. 10)', 1, 10, on_change=resetar_quiz)
+    gerar_novo_quiz = st.button("Gerar novo Quiz", on_click=gerar_quiz, args=(n_perguntas,), use_container_width=True)
 with col2:
-    st.write('Questões corretas: ', sst.corretas)
-with col3:
-    gerar_novo_quiz = st.button("Gerar novo Quiz", on_click=gerar_quiz)
+    st.write(f'Questões corretas: {sst.corretas}')
 
 
 if sst.quiz:
-    botoes = st.columns(5)
+    botoes = st.columns(n_perguntas)
     for indice_botao in range(len(botoes)):
         with botoes[indice_botao]:
             st.button(f'{indice_botao+1}', key=f"botao{indice_botao+1}", use_container_width=True)
 
 
-    for numero_questao in range(1,6):
+    for numero_questao in range(1,len(botoes)+1):
         atributo = "botao" + f"{numero_questao}"
         if sst[f"{atributo}"] == True:
             sst.questao_atual = numero_questao
--- quiz.py
+++ quiz.py
@@ -1,14 +1,14 @@
 import json
 from random import shuffle
 
-def quizGenerator() -> list:
+def quizGenerator(n_perguntas=5) -> list:
     with open('perguntas.json', 'r', encoding='utf-8') as arquivo:
         perguntas = json.load(arquivo)
-    
     shuffle(perguntas["perguntas"])
-
-    # print(perguntas["perguntas"][:5])
-    return perguntas["perguntas"][:5]
+    if n_perguntas > 10:
+        return perguntas["perguntas"][:10]
+    else:
+        return perguntas["perguntas"][:n_perguntas]
 
 
 if __name__ == '__main__':
