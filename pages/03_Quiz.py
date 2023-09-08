00e1a9e6b356b518f51e6bbfe4ebd843f3f76055
Alexandre Daltro <alexandrebvd@gmail.com>
2023-09-08 12:36:46 -0300

    Quiz como formulario e contagem de pontos

--- pages/03_Quiz.py
+++ pages/03_Quiz.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import quiz as q
+import json
 
 
 sst = st.session_state
@@ -11,11 +12,11 @@ if 'corretas' not in sst:
 if 'perguntas' not in sst:
     sst.perguntas = []
 
-if 'questao_atual' not in sst:
-    sst.questao_atual = 1
-
 if 'quiz' not in sst:
     sst.quiz = False
+
+if 'preenchido' not in sst:
+    sst.preenchido = False
 
 
 # Define função que gera o quiz com n perguntas escolhidas aleatórias do arquivo json.
@@ -24,49 +25,63 @@ def gerar_quiz(n_perguntas=5):
     sst.perguntas = perguntas
     sst.corretas = 0
     sst.quiz = True
-    return
+    sst.preenchido = False
 
 
 def resetar_quiz():
     sst.quiz = False
+    sst.preenchido = False
     sst.corretas = 0
 
 
-def verifica_alternativa():
-    for item in sst.items():
-        if item[1] and item[0] == sst.perguntas[int(sst.questao_atual) - 1]["resposta_correta"]:
-            sst.corretas += 1
+def verifica_alternativas():
+    sst.corretas = 0
+    for key in sst.keys():
+        if "questao" in key:
+            numero_questão = int(key.split("_")[1])
+            if sst[key] == sst.perguntas[numero_questão - 1]["resposta_correta"]:
+                sst.corretas += 1
 
 
-def renderiza_questao(n):
-    st.subheader(sst.perguntas[int(f"{n-1}")]["pergunta"])
-    for alternativa in sst.perguntas[int(f"{n-1}")]["alternativas"]:
-        st.button(alternativa, use_container_width=True, key=alternativa, on_click=verifica_alternativa)
+def verifica_preenchimento():
+    sst.questoes_preenchidas = 0
+    sst.preenchido = False
+    for key in sst.keys():
+        if "questao" in key and sst[key] != "":
+            sst.questoes_preenchidas += 1
+    if sst.questoes_preenchidas == sst.n_perguntas:
+        sst.preenchido = True
+        verifica_alternativas()
+    else:
+        st.warning("Preencha todas as questões antes de submeter o quiz.")
 
 
 st.title("Vamos treinar os conceitos sobre educação financeira!")
-col1, col2 = st.columns(2)
-with col1:
-    n_perguntas = st.number_input('Número de questões (Máx. 10)', 1, 10, on_change=resetar_quiz)
-    gerar_novo_quiz = st.button("Gerar novo Quiz", on_click=gerar_quiz, args=(n_perguntas,), use_container_width=True)
-with col2:
-    st.write(f'Questões corretas: {sst.corretas}')
+n_perguntas = st.number_input('Número de questões (Máx. 10)', 1, 10, value=5, on_change=resetar_quiz, key="n_perguntas")
+gerar_novo_quiz = st.button("Gerar novo Quiz", on_click=gerar_quiz, args=(n_perguntas,), use_container_width=True)
 
 
 if sst.quiz:
-    botoes = st.columns(n_perguntas)
-    for indice_botao in range(len(botoes)):
-        with botoes[indice_botao]:
-            st.button(f'{indice_botao+1}', key=f"botao{indice_botao+1}", use_container_width=True)
-
-
-    for numero_questao in range(1,len(botoes)+1):
-        atributo = "botao" + f"{numero_questao}"
-        if sst[f"{atributo}"] == True:
-            sst.questao_atual = numero_questao
-
-if sst.quiz:
-    renderiza_questao(sst.questao_atual)
+    ans = []
+    mark = 0
+    with st.form(key = "quiz_form"):
+        for i, questao in enumerate(sst.perguntas):
+            # Tonar a primeira label invisível para que o radio button não fique selecionado
+            st.markdown(
+                        """
+                    <style>
+                        div[role=radiogroup] label:first-of-type {
+                            visibility: hidden;
+                            height: 0px;
+                        }
+                    </style>
+                    """,
+                        unsafe_allow_html=True,
+                    )
+            st.radio(f'Q{i+1}: {questao["pergunta"]}', options=[""]+questao["alternativas"], key=f'questao_{i+1}')
+        submitted = st.form_submit_button(label='Submit', on_click=verifica_preenchimento)
+        if sst.preenchido:
+            st.success(f"Você acertou {sst.corretas} de {sst.n_perguntas}.")
 
 # Se quiser ver as variáveis guardadas a cada rerun é só descomentar a próxima linha
 # sst
