08467b7df2117a626025a56e687c79f82fbaa17c
aragao <acrafilho@gmail.com.br>
2023-09-10 12:34:07 -0300

    adicionado logo - precisa melhorar

--- projeto_aplicado_XP.py
+++ projeto_aplicado_XP.py
@@ -1,9 +1,18 @@
 import streamlit as st
 
 def main():
-    st.title("Projeto Aplicado - Faculdade XP Educação")
-    st.subheader("Selecione a opção ao lado")
-  
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.write(' ')
 
+    with col2:
+        st.image('logo - bing.jpg', width=100)
+
+    with col3:
+        st.write(' ')
+        
+    st.title("Projeto Aplicado - Faculdade XPE")
+  
 if __name__ == "__main__":
     main()
