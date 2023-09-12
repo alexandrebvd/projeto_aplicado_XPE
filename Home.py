ac9cfb93dd16dff5e1a76c1c1643e27a1f96b5d1
Alexandre Daltro <alexandrebvd@gmail.com>
2023-09-11 21:38:42 -0300

    ajuste tela inicial

--- Home.py
+++ Home.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
 def main():
-    st.title("[Nome do app]")
+    st.title("Bem-vindo(a) ao FinanceiraMente")
+    st.write("Projeto Aplicado - Faculdade XPE")
+    st.image('logo - bing.jpg', use_column_width='auto')
     st.subheader("Nosso objetivo é facilitar o seu aprendizado sobre educação financeira.")
 
     col1, col2 = st.columns(2)
-    col1.button("Entrar")
-    col2.button("Criar nova conta")
+    col1.button("Entrar", use_container_width=True)
+    col2.button("Criar nova conta", use_container_width=True)
 
 if __name__ == "__main__":
     main()
