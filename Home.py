1048a1b2137ab2ea4c34d7634d53122ab13fb3b2
Adriano Henrique <adriano.henrique.eng@gmail.com>
2023-09-20 19:51:28 -0300

    Incl. logo e formatação da home

--- Home.py
+++ Home.py
@@ -8,12 +8,14 @@ st.set_page_config(page_title="FinanceiraMente", page_icon = img)
 def main():
     st.title("Bem-vindo(a) ao FinanceiraMente")
     st.write("Projeto Aplicado - Faculdade XPE")
-    st.image('logo - bing.jpg', use_column_width='auto')
+    col1, col2, col3 = st.columns(3)
+    with col2:
+        st.image('financeiraMente__1_-removebg-preview.png', width=380)
     st.subheader("Nosso objetivo é facilitar o seu aprendizado sobre educação financeira.")
 
-    col1, col2 = st.columns(2)
-    col1.button("Entrar", use_container_width=True)
-    col2.button("Criar nova conta", use_container_width=True)
+    col4, col5 = st.columns(2)
+    col4.button("Entrar", use_container_width=True)
+    col5.button("Criar nova conta", use_container_width=True)
     st.markdown(f"<p style='color:#ff0000;font-size:24px;'>Funcionalidade de login em construção.</p>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
