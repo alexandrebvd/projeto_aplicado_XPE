88bfc04db0d522f8d4fe2bf5b18aba95072a186e
Adriano Henrique <adriano.henrique.eng@gmail.com>
2023-09-20 19:58:00 -0300

    Aplicação tema na pág, Conceitos

--- pages/01_Conceitos_Financeiros.py
+++ pages/01_Conceitos_Financeiros.py
@@ -4,6 +4,68 @@ from PIL import Image
 img = Image.open('fm-favicon.png')
 st.set_page_config(page_title="FinanceiraMente", page_icon = img)
 
+dark = '''
+<style>
+    .stApp {
+    background-color: #0E1117;
+    color: #FAFAFA;
+    }
+    .block-container {
+    background-color: #0E1117;
+    color: #FAFAFA;
+    }
+    li {
+    color: #FAFAFA;
+    }
+    h1, span {
+    color: #FAFAFA;
+    }
+    .element-container {
+    color: #FAFAFA;
+    }
+    .stButton {
+    color: #31333F;
+    }
+    .row-widget {
+    color: #31333F
+    }
+    p {
+    color: #FAFAFA;
+    }
+    
+</style>
+'''
+
+light = '''
+<style>
+    .stApp {
+    background-color: #FFFFFF;
+    color: #31333F
+    }
+</style>
+'''
+st.markdown(light, unsafe_allow_html=True)
+
+# Create a toggle button
+# toggle = st.button("Modo :sun_with_face:/ :new_moon_with_face:")
+
+# Use a global variable to store the current theme
+if "theme" not in st.session_state:
+    st.session_state.theme = "light"
+
+# Change the theme based on the button state
+# if toggle:
+    if st.session_state.theme == "light":
+        st.session_state.theme = "dark"
+    else:
+        st.session_state.theme = "light"
+
+# Apply the theme to the app
+if st.session_state.theme == "dark":
+    st.markdown(dark, unsafe_allow_html=True)
+else:
+    st.markdown(light, unsafe_allow_html=True)
+
 st.title("Conceitos Financeiros")
 st.subheader("Nesta seção vamos entender mais sobre conceitos e jargões que permeiam esta área!")
 
